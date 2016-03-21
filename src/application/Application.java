d7235409e362b4ad0bb859db365b7669f0f6b68f
Andrew Henrique <andrewhenriquef@gmail.com>
2016-03-21 09:59:10 -0300

    added pizzaria ibate

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,8 +1,8 @@
 package application;
 
 import fabricapizzaria.Pizzaria;
+import fabricapizzaria.PizzariaIbate;
 import fabricapizzaria.PizzariaSaoCarlos;
-
 import pizza.Pizza;
 
 public class Application {
@@ -12,6 +12,14 @@ public class Application {
 		Pizzaria pizzaria = new PizzariaSaoCarlos();
 		Pizza pizza;
 		pizza = pizzaria.criaPizza("calabresa");
+		
+		System.out.println(pizza.getNome());
+		
+		
+		pizzaria = new PizzariaIbate();
+		pizza = pizzaria.criaPizza("mussarela");
+		
+		
 		
 		System.out.println(pizza.getNome());
 		
