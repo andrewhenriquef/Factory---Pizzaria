69f3f86a62aedd362086608478f68f56abb8e62e
Andrew Henrique <andrewhenriquef@gmail.com>
2016-03-21 10:01:30 -0300

    Added pizzaria IBATE

--- src/fabricapizzaria/PizzariaIbate.java
+++ src/fabricapizzaria/PizzariaIbate.java
@@ -1,22 +1,31 @@
 package fabricapizzaria;
 
 import pizza.Pizza;
+import pizza.PizzaCalabresaEstiloIbate;
+import pizza.PizzaMussarelaEstiloIbate;
 
-public class PizzariaIbate{ //extends Pizzaria {
+public class PizzariaIbate extends Pizzaria {
 
 	
 	public Pizza realizaPedido(Pizza pizza) {
-		
+		// TODO Auto-generated method stub
 		return pizza;
-		
 	}
 
-/*
+
 	@Override
 	public Pizza criaPizza(String tipo) {
-		// TODO Auto-generated method stub
+
+		if (tipo.equals("mussarela")){ 
+			Pizza pizza = new PizzaMussarelaEstiloIbate();
+			return pizza;
+				
+		} else if(tipo.equals("calabresa")) {
+			Pizza pizza = new PizzaCalabresaEstiloIbate();
+			return pizza;
+		}
 		
-		return pizza;
+		return null;
+
 	}
-*/
 }
